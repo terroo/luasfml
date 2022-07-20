e6491cd82e3e6dbf19eed2eb09614e71bc64403f
Marcos Oliveira <marcos@terminalroot.com.br>
2022-07-20 12:43:02 -0300

    change font and indentation to event

--- assets/main.lua
+++ assets/main.lua
@@ -4,7 +4,7 @@ clearColor = sfColor.new(0,0,0);
 colorWhite = sfColor.new(255, 255, 255)
 
 font = sfFont.new()
-font:loadFromFile("/usr/share/fonts/liberation-fonts/LiberationMono-Regular.ttf")
+font:loadFromFile("./font.ttf")
 
 message = sfText.new("This is Lua SFML!",font,40);
 message:setPosition( 200, 250);
@@ -13,7 +13,8 @@ message:setColor(colorWhite)
 while window:isOpen() do
   event = sfEvent.new();
   while window:pollEvent(event) do
-    if(event:type() == sfEventType.Closed) then window:close(); 
+    if(event:type() == sfEventType.Closed) then 
+      window:close(); 
     end
   end
   window:clear(clearColor);
